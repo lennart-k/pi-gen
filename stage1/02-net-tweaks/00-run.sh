7c540da5f709c698c1a7a9d89949fd99c641b222
lennart-k <18233294+lennart-k@users.noreply.github.com>
2019-07-04 22:35:40 +0200

    :bug: Fixed networking with eth0 and the hostname

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
 install -m 644 files/ipv6.conf "${ROOTFS_DIR}/etc/modprobe.d/ipv6.conf"
+install -m 644 files/interfaces "${ROOTFS_DIR}/etc/network/interfaces"
+
 echo $HOSTNAME > "${ROOTFS_DIR}/etc/hostname"
+echo "127.0.1.1    ${HOSTNAME}" > "${ROOTFS_DIR}/etc/hosts"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
+
+on_chroot << EOF
+systemctl enable networking
+EOF
